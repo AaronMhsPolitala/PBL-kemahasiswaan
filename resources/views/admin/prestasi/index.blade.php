24bbc94a78f28e248017a415dba7b163bf4f44ef
Aron <Aron@gmail.com>
2025-10-12 13:00:00 +0700

    Update aspirasi konten

---
+++ resources/views/admin/prestasi/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+
+@section('title', 'Admin Prestasi')
+
+@section('content')
+    <h1>Admin Prestasi Index</h1>
+    <p>This is the empty admin prestasi index page.</p>
+@endsection
---
+++ resources/views/pengurus/berita/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.pengurus')
+
+@section('title', 'Pengurus Berita')
+
+@section('content')
+    <h1>Pengurus Berita Index</h1>
+    <p>This is the empty pengurus berita index page.</p>
+@endsection
